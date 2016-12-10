77d61b5e04e526c54d6f0e363428f0eb1fa12d9b
Vitor <vitorvqz@gmail.com>
2016-12-10 19:17:24 -0300

    titulo

--- iluminar.php
+++ iluminar.php
@@ -89,7 +89,7 @@ header('Content-Type: text/html; charset=utf-8');
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<title>Teste</title>
+	<title>Iluminar</title>
 	<style>
 		
 		.green{
